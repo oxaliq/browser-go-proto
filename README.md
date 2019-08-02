8f07e5bfcd9346487d8ace61cc2793c2995f1570
Sorrel Bri <sorrel@SBJ-comp.local>
2019-08-02 14:14:23 -0700

    add data structures, basic style

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 #### Minimum Deliverable Product
 
 a working game of go for a 9x9 board that
-* displays well on mobile
+* displays well on mobile or desktop
 * initiates a game with suggested handicap and komi according to rank input
 * * displays how to play in open screen
 * lets the user know whose turn it is
@@ -11,7 +11,6 @@ a working game of go for a 9x9 board that
 * * implement a search algorithm to avoid moving into dead space
 * correctly removes captured stones and adds them to capturing player's score
 * logs game record
-* maintains a one move game state history for 'undo mismove'
 * allows players to pass or resign
 * * ends game upon 2 consecutive passes
 * calculates estimated score at game end
@@ -21,6 +20,7 @@ a working game of go for a 9x9 board that
 
 stretch goals
 * uses stone placement GUI for resign and pass
+* maintains a one move game state history for 'undo mismove'
 * converts string to .sgf format
 * allows users to edit game info mid game
 * add stone placement sounds
--- js/main.js
+++ js/main.js
@@ -1,58 +1,130 @@
-// global constants declared
+/*----- constants -----*/
+// game state object {gameMeta object, playerMeta object, turn, pass, gameRecord, bCaptures, wCaptures}
+const gameState = {
+  winner: null,
+  turn: null, // turn logic depends on handicap stones
+  pass: null,
+  komi: null, // komi depends on handicap stones
+  handicap: null,
+  playerState: {
+    bCaptures: null,
+    wCaptures: null
+  },
+  gameMeta: { // declared at game start and not editable after
+    date: null// contains metadata 
+  },
+  playerMeta: { // editable during game
+    b: {
+      name: null,
+      rank: null, 
+      rankCertain: false 
+    },
+    w: {
+      name: null,
+      rank: null,
+      rankCertain: false
+    },
+  },
+  gameRecord : []
+}
 
-    // boardState, lastState, overlay (copy of boardState holding pre-move object, game end group state) 
-    // bCaptures, wCaptures, gameInfo object, playerInfo object, turn, pass, gameRecord, 
-    // handicapStones, deadShapes{}
+    // boardState [point objects-contain overlay] lastState (created from boardState)
+    // groups? 
+    // deadShapes{}
 
+// index represents handicap placement, eg handiPlace[1] = { (3, 3), (7, 7) }
+const handiPlace = [ 0, 
+  [ { rPos: 3, cPos: 3 }, { rPos: 7, cPos: 7 } ], 
+  [ { rPos: 3, cPos: 3 }, { rPos: 7, cPos: 7 }, { rPos: 3, cPos: 7 } ], 
+  [ { rPos: 3, cPos: 3 }, { rPos: 7, cPos: 7 }, { rPos: 3, cPos: 7 }, { rPos: 7, cPos: 3 } ]];
+
+/*----- app's state (variables) -----*/
 
 // define initial game state
+let boardState;
 
-    // define boardState and overlay as 2d 9x9 arrays
-    // boardState accepts values of 0, 1, -1
-    // overlay accepts values of 0, 1, -1, 'k', 'd', 'chk', 'hold', 'l', 'x'
-        // 'k' represents komi, in-game integers represent move previews, 
-        // 'chk', 'hold', 'x' and 'l' represent points checked during checkLegalMove run
-        // game-end integer represent points of territory, 'd' represents dame,
+// Class Point {
+//     rPos: 1,
+//     cPos: 1,
+//     neighbors: {
+//       top: null,
+//       btm: null,
+//       lft: null,
+//       rgt: null
+//     }
+//     checkLegal: function() {
+      
+//     }
+//   }
 
 
-// cached elements
-    // store #menu for displaying game info
-    // store 
+  // modeling 1,1 point for 
+  // define boardState and overlay as 2d 9x9 arrays
+  // boardState accepts values of 0, 1, -1
+  // overlay accepts values of 0, 1, -1, 'k', 'd', 'chk', 'hold', 'l', 'x'
+  // 'k' represents komi, in-game integers represent move previews, 
+  // 'chk', 'hold', 'x' and 'l' represent points checked during checkLegalMove run
+  // game-end integer represent points of territory, 'd' represents dame,
+  
+  
+  /*----- cached element references -----*/
+  // store #menu for displaying game info
+  // store 
+  
+  
+  /*----- event listeners -----*/
+  // input listeners for player names, ranks, rank certainty (editable during game)
+  //input lister for handicap + komi (only editable pre-game)
+  // ::hover-over on board to preview move (with legal move logic)
+  // click on board to play move
+  // ::hover-over on either bowl for pass, one-level undo options (CSS implementation)
+  // click on menu items 
+  // click on kifu to display game menu
+  
+  /*----- functions -----*/
+init();
 
+  function init() {
+    gameState.winner = null;
+    // gameState.turn = ? : ; // get turn from consequences of player input
+    gameState.pass = null;
+    // gameState.komi = ; // get komi from player input
+    // gameState.handicap = ; // get handicap from player input
+    gameState.playerState.bCaptures = 0;
+    gameState.playerState.wCaptures = 0;
+    // gameState.gameMeta.date = // get from browser window
+    // get any future meta from player input
+    // gameState.playerMeta.b // get from player input
+    // gameState.playerMeta.w // get from player input
+    gameState.gameRecord = []; // clear game record from previous game
+    // gameState.boardState // create board from user input
+  };
+    
 
-// set event listeners
-    // input listeners for player names, ranks, rank certainty (editable during game)
-        //input lister for handicap + komi (only editable pre-game)
-    // ::hover-over on board to preview move (with legal move logic)
-    // click on board to play move
-    // ::hover-over on either bowl for pass, one-level undo options (CSS implementation)
-    // click on menu items 
-    // click on kifu to display game menu
-
-// functions
-    // initialize game
-        // set handicap stones
-    // render
-        // render board
-            //render moves
-            //render preview
-        // render captures
-        // render player turn marker
-        // game-end
-            // render dead group suggestion 
-            // render territory counts
-    // checkLegalMove
-        // clear overlay
-        // if move is not '0', move is illegal (opposing player or 'k' for ko)
-        // iterate through neighboring points in clockwise order
-            // if anyone is '0' move is legal - call render preview
-            // if neighboring point is opposing player
-                // cycle through opposing player group marking points as overlay: 'chk' when checked and 
-                    // overlay: 'hold' if they are neighboring points of opposing player color
-                // if any neighboring point is '0' terminate cycle and move to next neighboring point of original move
-                // if there are unchecked points of 'hold' return
-                // if no boardState: 0 points, move is legal overlay: 'l'
-                // set all 'chk' to 'x' to represent stones that will be captured upon move
+  // functions
+  // initialize game
+  // set handicap stones
+  // render
+  // render board
+  //render moves
+  //render preview
+  // render captures
+  // render player turn marker
+  // game-end
+  // render dead group suggestion 
+  // render territory counts
+  // checkLegalMove
+  // clear overlay
+  // if move is not '0', move is illegal (opposing player or 'k' for ko)
+  // iterate through neighboring points in clockwise order
+  // if anyone is '0' move is legal - call render preview
+  // if neighboring point is opposing player
+  // cycle through opposing player group marking points as overlay: 'chk' when checked and 
+  // overlay: 'hold' if they are neighboring points of opposing player color
+  // if any neighboring point is '0' terminate cycle and move to next neighboring point of original move
+  // if there are unchecked points of 'hold' return
+  // if no boardState: 0 points, move is legal overlay: 'l'
+  // set all 'chk' to 'x' to represent stones that will be captured upon move
             // if neighboring point is player's
                 // cycle through player group marking points as overlay: 'chk' || 'hold'
                 // if any neighboring point is '0' ternminate cycle and mark point as 'l'
